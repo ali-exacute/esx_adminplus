223c85c5ab654cd2455e4ed19fec00b1d9a710ab
ali-exacute <alinikbakht.98ia@gmail.com>
2020-05-10 14:46:04 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,14 +3,14 @@ Config.Locale = 'en'
 Config.reportCooldown = 60 --seconds
 Config.warnMax = 3  --how many warn player can get before getting kicked?
 Config.adminRanks = { -- change this as your server ranking ( default are : superadmin | admin | moderator )
-				--superadmin,
-				--admin,
-				--moderator,
-				'jradmin',
-				'sradmin',
-				'headadmin',
+				'superadmin',
+				'admin',
 				'moderator',
-				'manager',
-				'owner',
-				'developer',
+				--'jradmin',
+				--'sradmin',
+				--'headadmin',
+				--'moderator',
+				--'manager',
+				--'owner',
+				--'developer',
             }
