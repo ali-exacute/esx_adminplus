2476f3e9f0779b2be97954491f9bf2db09d5887b
ali-exacute <alinikbakht.98ia@gmail.com>
2020-05-10 15:13:36 +0430

    fixed exclude not working as intended

--- server.lua
+++ server.lua
@@ -366,11 +366,18 @@ function havePermission(xPlayer, exclude)	-- you can exclude rank(s) from having
 	if exclude and type(exclude) ~= 'table' then exclude = nil;print("^3[esx_admin] ^1ERROR ^0exclude argument is not table..^0") end	-- will prevent from errors if you pass wrong argument
 
 	local playerGroup = xPlayer.getGroup()
-	if Config.adminRanks[playerGroup] then
-		if not exclude then
-			return true
-		elseif exclude[playerGroup] then
-			return false
+	for k,v in pairs(Config.adminRanks) do
+		if v == playerGroup then
+			if not exclude then
+				return true
+			else
+				for a,b in pairs(exclude) do
+					if b == v then
+						return false
+					end
+				end
+				return true
+			end
 		end
 	end
 	return false
