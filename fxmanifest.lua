7be28450f9392d878693585209d2a1d301adf0bc
G1itchss <67972307+G1itchs@users.noreply.github.com>
2020-12-24 11:20:55 -0500

    - I'm an idiot (Update fxmanifest again)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,13 +6,16 @@ version '1.1.0'
 
 shared_scripts {
     '@es_extended/locale.lua',
-    '@mysql-async/lib/MySQL.lua',
     'locales/en.lua',
     'locales/es.lua',
     'locales/fa.lua',
     'config.lua'
 }
 client_script 'client.lua'
-server_script 'server.lua'
+server_scripts {
+    'server.lua',
+    '@mysql-async/lib/MySQL.lua'
+
+}
 
 dependency "es_extended"
